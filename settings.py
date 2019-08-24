017ce8914d4221b554972d3e3893b8ed7c6d1139
Amir Nadiri <amir77ni@gmail.com>
2019-08-24 23:59:09 +0800

    relation

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 BOOSTRAP_SERVER="172.17.0.1:9092"
-BTC_BLOCK_TOPIC = 'bitcoin'
+KAFKA_TOPIC = ''
 
 CASSANDRA_HOST = os.environ.get('CASSANDRA_HOST', '172.17.0.1')
 CASSANDRA_KEYSPACE = 'bitcoin'
